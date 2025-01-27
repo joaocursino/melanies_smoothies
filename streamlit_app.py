b6925c9b1bb8f2176d066e76ceb13c27bc764998
joaocursino <93956704+joaocursino@users.noreply.github.com>
2025-01-26 23:31:41 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,7 +58,17 @@ if ingredients_list: # is not null
     #     st.success('Your Smoothie is ordered!', icon="✅")
 
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
-st.text(smoothiefroot_response. json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
+if ingredients_list:
+    ingredients_string = ''
+    
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
+
+# smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+# st.text(smoothiefroot_response)
+# st.text(smoothiefroot_response. json())
+# sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
