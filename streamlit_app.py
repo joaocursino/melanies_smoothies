5b384b0091130772a80ffbc56b51d10c68e972b3
joaocursino <93956704+joaocursino@users.noreply.github.com>
2025-01-26 23:29:04 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,3 +61,4 @@ if ingredients_list: # is not null
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 st.text(smoothiefroot_response)
 st.text(smoothiefroot_response. json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
